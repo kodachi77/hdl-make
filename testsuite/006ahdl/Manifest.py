909735ef896f24892173a01546a79acd4235e544
kodachi77 <12465849+kodachi77@users.noreply.github.com>
2024-03-09 10:04:59 -0500

    added testsuite

---
+++ testsuite/006ahdl/Manifest.py
@@ -0,0 +1,7 @@
+action = "simulation"
+
+sim_tool="active_hdl"
+
+top_module = "gatesv_tb"
+
+files = [ "../files/gate.vhdl", "../files/gate2.v", "../files/gatesv_tb.sv" ]
---
+++ testsuite/013libero/Manifest.py
@@ -0,0 +1,11 @@
+action = "synthesis"
+
+syn_tool="libero"
+syn_device="anfpga"
+syn_grade="3"
+syn_package="ff"
+syn_project="gate"
+
+top_module = "gate"
+
+files = [ "../files/gate.vhdl", "syn.sdc", "comp.pdc" ]
